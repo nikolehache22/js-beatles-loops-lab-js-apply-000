07dedc0dc3e11308ab18bbf1d5588043e3b5f79d
Nikole Hache <nh131215@gmail.com>
2019-06-25 15:38:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,17 +10,16 @@ function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0;
   while ( i < facts.length ) {
-    newFacts.push(facts[i] + "!!!");
+    facts.push(facts[i] + "!!!");
     i++;
     return facts;
   }
   var newFacts = ["foo","bar"];
-  while ( i < facts.length ) {
-    newFacts.push(facts[i] + "!!!");
+  while ( i < newFactsacts.length ) {
+    newFacts.push(newFactsacts[i] + "!!!");
     i++;
-    return facts;
+    return newFacts;
   }
-  return newFacts;
 }
 
 function iLoveTheBeatles(num) {
