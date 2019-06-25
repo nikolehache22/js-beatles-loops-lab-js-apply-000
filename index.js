7612ae9521eb666861aaf49b1685d1977d68570d
Nikole Hache <nh131215@gmail.com>
2019-06-25 15:45:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts() {
   var newFacts = [];
   var i = 0;
   while ( i < facts.length ) {
-    newFactsfacts.push(facts[i] + "!!!");
+    newFacts.push(facts[i] + "!!!");
     i++;
     return newfacts;
   }
